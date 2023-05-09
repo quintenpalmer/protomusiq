2ef9529385f1f11efc252c35d5729e557d3ee84c
Quinten Palmer <quintenpalmer@protonmail.com>
2023-05-09 15:54:51 -0400

    Rm unused const width, height, and page sizes

--- src/app/src/gui/view/page/consts.rs
+++ src/app/src/gui/view/page/consts.rs
@@ -1,7 +1,2 @@
-pub const GRID_LAYOUT_WIDTH: usize = 6;
-pub const GRID_LAYOUT_HEIGHT: usize = 4;
-
-pub const PAGE_SIZE: usize = GRID_LAYOUT_WIDTH * GRID_LAYOUT_HEIGHT;
-
 pub const ICON_STR_LENGTH: usize = 18;
 pub const NAV_STR_LENGTH: usize = 16;
