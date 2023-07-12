286901605064ad3e5cc6a752f1238b868b940de4
Quinten Palmer <quintenpalmer@protonmail.com>
2023-07-11 22:21:07 -0400

    Copy to buildexec directory as part of deploy
    This "project" is a syncthing target for me, where it holds just the
    compiled binaries I need to run this project. It's a bit hacky, but it's
    working for now. The real end game is to have this packaged up as
    something I can install with pacman or something, but I'm not at that
    scale yet, I claim.
    
    Note that this other "project" does have its own `install.sh` script
    that is a copy-paste of the %USERNAME% sed, which I'm just admitting as
    a DRY-fail for the moment.

--- src/app/deploy.sh
+++ src/app/deploy.sh
@@ -8,6 +8,8 @@ cargo doc
 cargo build --release
 
 cp ../../target/release/musiqapp ~/.bin/
+cp ../../target/release/musiqapp ~/storage/projects/buildexecs/musiqapp/
+cp assets/Musiqapp.desktop       ~/storage/projects/buildexecs/musiqapp/
 
 sed -e "s|%USERNAME%|$HOME|g" assets/Musiqapp.desktop > ~/.local/share/applications/Musiqapp.desktop
 update-desktop-database ~/.local/share/applications/
