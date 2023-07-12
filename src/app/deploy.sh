4f6e0c49cecfd2406bd6c5b14f52c11103c8ab17
Quinten Palmer <quintenpalmer@protonmail.com>
2023-07-11 22:20:28 -0400

    Also install desktop file as part of deploy script

--- src/app/deploy.sh
+++ src/app/deploy.sh
@@ -8,4 +8,8 @@ cargo doc
 cargo build --release
 
 cp ../../target/release/musiqapp ~/.bin/
+
+sed -e "s|%USERNAME%|$HOME|g" assets/Musiqapp.desktop > ~/.local/share/applications/Musiqapp.desktop
+update-desktop-database ~/.local/share/applications/
+
 #~/.bin/musiqapp
