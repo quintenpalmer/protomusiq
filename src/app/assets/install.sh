dc5782e118e6db45a2f7a6ec755a84c7c8ed5d55
Quinten Palmer <quintenpalmer@protonmail.com>
2023-07-16 14:45:54 -0400

    Include icon for musiqapp application desktop

--- src/app/assets/install.sh
+++ src/app/assets/install.sh
@@ -2,5 +2,8 @@ set -x
 
 cp assets/musiqapp ~/.bin/
 
+mkdir -p ~/.local/share/icons/hicolor/48x48/apps/
+cp assets/musiqapp.png ~/.local/share/icons/hicolor/48x48/apps/
+
 sed -e "s|%USERNAME%|$HOME|g" assets/Musiqapp.desktop > ~/.local/share/applications/Musiqapp.desktop
 update-desktop-database ~/.local/share/applications/
