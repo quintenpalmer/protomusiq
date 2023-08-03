c583f367a6dd6cc96ff862d90e058c9299d1bc24
Quinten Palmer <quintenpalmer@protonmail.com>
2023-08-02 22:30:36 -0400

    Add ytimporter tool
    This uses `html_parser` to actually read the html.

---
+++ src/ytimporter/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs;
+
+fn main() {
+    println!("Hello, world!");
+    let html_str = fs::read_to_string("imports/test.html").unwrap();
+    let html_dom = html_parser::Dom::parse(html_str.as_str()).unwrap();
+    for child in html_dom.children.iter() {
+        println!("child: {:?}", child);
+    }
+    println!("Goodbye.");
+}
