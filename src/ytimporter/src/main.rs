3b93cbf6044d52005052bb5d30a4edf3534f0c1b
Quinten Palmer <quintenpalmer@protonmail.com>
2023-08-04 20:26:33 -0400

    Make ytimporter just show the type of the top node
    Instead of trying to debug print the whole tree, which would take almost
    actually forever (I let it run for 5 hours when I first wrote it and it
    didn't finish in that time).

--- src/ytimporter/src/main.rs
+++ src/ytimporter/src/main.rs
@@ -4,8 +4,9 @@ fn main() {
     println!("Hello, world!");
     let html_str = fs::read_to_string("imports/test.html").unwrap();
     let html_dom = html_parser::Dom::parse(html_str.as_str()).unwrap();
-    for child in html_dom.children.iter() {
-        println!("child: {:?}", child);
-    }
+    println!("type: {:?}", html_dom.tree_type);
+    //for child in html_dom.children.iter() {
+    //    println!("child: {:?}", child);
+    //}
     println!("Goodbye.");
 }
