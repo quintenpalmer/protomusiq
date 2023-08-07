6ec6e1a3162a1962060ce4ccaeec4821d7054525
Quinten Palmer <quintenpalmer@protonmail.com>
2023-08-07 15:23:45 -0400

    Rename to `simple_debug_display`

--- src/ytimporter/src/main.rs
+++ src/ytimporter/src/main.rs
@@ -9,18 +9,18 @@ fn main() {
     let html_dom = html_parser::Dom::parse(html_str.as_str()).unwrap();
     println!("type: {:?}", html_dom.tree_type);
     for child in html_dom.children.iter() {
-        explore_node(&child, 0)
+        simple_debug_display(&child, 0)
     }
     println!("Goodbye.");
 }
 
-fn explore_node(node: &html_parser::Node, indent_level: usize) {
+fn simple_debug_display(node: &html_parser::Node, indent_level: usize) {
     match node {
         html_parser::Node::Text(t) => println!("{}\"{}\"", "    ".repeat(indent_level), t.trim()),
         html_parser::Node::Element(e) => {
             println!("{}<{}>", "    ".repeat(indent_level), e.name);
             for child in e.children.iter() {
-                explore_node(child, indent_level + 1);
+                simple_debug_display(child, indent_level + 1);
             }
             println!("{}<\\{}>", "    ".repeat(indent_level), e.name);
         }
