771cb7af5e0deae446ddf882887f81cd0e61fecd
quintenpalmer@protonmail.com <Quinten Palmer>
2023-08-22 15:44:45 -0400

    Add unused `load_intermediate_tracks`

--- src/playhistoryimport/src/youtube/intermediate.rs
+++ src/playhistoryimport/src/youtube/intermediate.rs
@@ -18,3 +18,13 @@ pub fn save_intermediate_artists(artist_map: &BTreeMap<String, String>) {
         fs::File::create("youtube/output/debug/1_manual_artist_mapping.json").unwrap();
     serde_json::to_writer_pretty(manual_mapping_file_file, artist_map).unwrap();
 }
+
+pub fn load_intermediate_tracks() -> BTreeMap<String, musiqlibrary::TrackUniqueIdentifier> {
+    let manual_mapping_file_reader =
+        fs::File::open("youtube/intermediate/manual_track_mapping.json").unwrap();
+
+    let existing_manual_mapping: BTreeMap<String, musiqlibrary::TrackUniqueIdentifier> =
+        serde_json::from_reader(manual_mapping_file_reader).unwrap();
+
+    existing_manual_mapping
+}
