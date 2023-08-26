39a6b52ea5474ee4fd9afa6194e6da7373988539
quintenpalmer@protonmail.com <Quinten Palmer>
2023-08-26 15:10:39 -0400

    Actually skip prehistory files if not in configured allow list

--- src/app/src/datastore/jsonbacked/prehistory.rs
+++ src/app/src/datastore/jsonbacked/prehistory.rs
@@ -65,32 +65,38 @@ pub fn compute_historical_map(
     app_data_path: &path::PathBuf,
     allowed_prehistory_files: &Option<Vec<path::PathBuf>>,
 ) -> Vec<model::PrehistoryRecord> {
+    let allowed_files = common::FileAllower::new(allowed_prehistory_files);
+
     let mut records = Vec::new();
 
     for historical_file in fs::read_dir(app_data_path.join("data").join("prehistory")).unwrap() {
         let historical_file = historical_file.unwrap().path();
         println!("prehistory play count file: {:?}", historical_file);
-        match historical_file.extension().map(|x| x.to_str()) {
-            Some(Some("json")) => {
-                let container: PreHistoryContainer =
-                    common::maybe_get_existing_raw_data(&historical_file).unwrap();
+        if allowed_files.is_allowed(&historical_file) {
+            match historical_file.extension().map(|x| x.to_str()) {
+                Some(Some("json")) => {
+                    let container: PreHistoryContainer =
+                        common::maybe_get_existing_raw_data(&historical_file).unwrap();
 
-                let current_tracks = container.records;
+                    let current_tracks = container.records;
 
-                for entry in current_tracks.into_iter() {
-                    let track_unique_id =
-                        musiqlibrary::TrackUniqueIdentifier::from_track(&entry.track);
+                    for entry in current_tracks.into_iter() {
+                        let track_unique_id =
+                            musiqlibrary::TrackUniqueIdentifier::from_track(&entry.track);
 
-                    records.push(model::PrehistoryRecord {
-                        source: container.name.clone(),
-                        key: track_unique_id,
-                        count: entry.play_count,
-                    });
+                        records.push(model::PrehistoryRecord {
+                            source: container.name.clone(),
+                            key: track_unique_id,
+                            count: entry.play_count,
+                        });
+                    }
                 }
+                Some(Some(ext)) => println!("skipping non json file ({}) in data/tracker/", ext),
+                Some(None) => println!("skipping non json file in data/tracker/"),
+                None => println!("skipping non json file in data/tracker/"),
             }
-            Some(Some(ext)) => println!("skipping non json file ({}) in data/tracker/", ext),
-            Some(None) => println!("skipping non json file in data/tracker/"),
-            None => println!("skipping non json file in data/tracker/"),
+        } else {
+            println!("skipping prehistory file, per config");
         }
     }
     records
