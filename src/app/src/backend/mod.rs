46f5822e2073cc05ca8699b89192d6a8017e86a8
Quinten Palmer <quintenpalmer@protonmail.com>
2023-09-21 16:22:37 -0400

    Extract backend playback to seperate module

--- src/app/src/backend/mod.rs
+++ src/app/src/backend/mod.rs
@@ -8,6 +8,8 @@ use crate::shared;
 
 use crate::services::{mpris, sink, tracker};
 
+mod playback;
+
 pub fn create_backend_with_client_and_callback(
     config_state: model::app::AppConfigState,
     loader: loader::Loader,
@@ -75,7 +77,7 @@ pub fn run_forever(
         match gui_rx.try_recv() {
             Ok(gui_msg) => match gui_msg {
                 shared::GUIToBackendMessage::BackendPlayback(to_playback_msg) => {
-                    handle_playback_request(
+                    playback::handle_playback_request(
                         &mut play_queue,
                         sink_client.clone(),
                         mpris_client.clone(),
@@ -111,7 +113,7 @@ pub fn run_forever(
                     shared::MprisCallbackMessage::Next => shared::PlaybackRequest::Next,
                 };
 
-                handle_playback_request(
+                playback::handle_playback_request(
                     &mut play_queue,
                     sink_client.clone(),
                     mpris_client.clone(),
@@ -139,7 +141,7 @@ pub fn run_forever(
                         play_queue.current_second += 1;
                     }
                     shared::SinkCallbackMessage::SongEnded => {
-                        handle_playback_request(
+                        playback::handle_playback_request(
                             &mut play_queue,
                             sink_client.clone(),
                             mpris_client.clone(),
@@ -163,183 +165,4 @@ pub fn run_forever(
     }
 
     println!("MULTI-BACKEND:\tdone listening");
-}
-
-fn handle_playback_request(
-    play_queue: &mut shared::PlayQueueInfo,
-    sink_client: shared::Client<shared::SinkMessage>,
-    mpris_client: shared::Client<shared::MprisMessage>,
-    tracker_client: shared::Client<shared::TrackerMessage>,
-    internal: shared::PlaybackRequest,
-) {
-    println!("GUI:\thandling internal: {:?}", internal);
-    match internal {
-        shared::PlaybackRequest::LoadCurrentSong => match play_queue.current_playback {
-            Some(ref outer_current_playback) => match outer_current_playback {
-                shared::PlayQueueEntry::Track(ref current_playback) => {
-                    let _ = sink_client
-                        .send(shared::SinkMessage::LoadSong(
-                            current_playback.track.metadata.path.clone(),
-                            play_queue.current_volume,
-                        ))
-                        .unwrap();
-                    let _ = mpris_client.send(shared::MprisMessage::SetMetadata(
-                        current_playback.track.metadata.album_artist.clone(),
-                        current_playback.track.metadata.title.clone(),
-                    ));
-                    let _ = tracker_client.send(shared::TrackerMessage::SongStarted(
-                        current_playback.track.clone(),
-                    ));
-                }
-                shared::PlayQueueEntry::Action(shared::PlayQueueAction::Pause) => {
-                    play_queue.playing = false;
-                    handle_playback_request(
-                        play_queue,
-                        sink_client,
-                        mpris_client,
-                        tracker_client,
-                        shared::PlaybackRequest::Pause,
-                    );
-                }
-            },
-            None => println!("Nothing to load for current song"),
-        },
-        shared::PlaybackRequest::PlaySongs(tracks) => handle_playback_request(
-            play_queue,
-            sink_client,
-            mpris_client,
-            tracker_client,
-            shared::PlaybackRequest::InsertSongs(tracks, true),
-        ),
-        shared::PlaybackRequest::AppendSongs(tracks, load_next) => {
-            let mut new_songs_to_queue = Vec::new();
-            for iter_track in tracks.into_iter() {
-                new_songs_to_queue.push(shared::PlayQueueEntry::Track(shared::PlayQueueTrack {
-                    track: iter_track,
-                }));
-            }
-            play_queue.play_queue.append(&mut new_songs_to_queue);
-            if load_next {
-                handle_playback_request(
-                    play_queue,
-                    sink_client,
-                    mpris_client,
-                    tracker_client,
-                    shared::PlaybackRequest::Next,
-                );
-            } else {
-                // Nothing else to run if not loading next
-            }
-        }
-        shared::PlaybackRequest::InsertSongs(tracks, load_next) => {
-            let mut new_songs_to_queue = Vec::new();
-            for iter_track in tracks.into_iter() {
-                new_songs_to_queue.push(shared::PlayQueueEntry::Track(shared::PlayQueueTrack {
-                    track: iter_track,
-                }));
-            }
-            new_songs_to_queue.append(&mut play_queue.play_queue);
-            play_queue.play_queue = new_songs_to_queue;
-
-            if load_next {
-                handle_playback_request(
-                    play_queue,
-                    sink_client,
-                    mpris_client,
-                    tracker_client,
-                    shared::PlaybackRequest::Next,
-                );
-            } else {
-                // Nothing else to run if not loading next
-            }
-        }
-        shared::PlaybackRequest::Prev => {
-            if play_queue.play_history.len() > 0 {
-                match play_queue.current_playback {
-                    Some(ref current_playback) => {
-                        let mut new_play_queue = vec![current_playback.clone()];
-                        new_play_queue.append(&mut play_queue.play_queue);
-                        play_queue.play_queue = new_play_queue;
-                    }
-                    None => (),
-                };
-                let track = play_queue.play_history.pop().unwrap();
-                play_queue.current_playback = Some(match track {
-                    shared::PlayQueueEntry::Track(ref t) => {
-                        shared::PlayQueueEntry::Track(shared::PlayQueueTrack {
-                            track: t.track.clone(),
-                        })
-                    }
-                    shared::PlayQueueEntry::Action(shared::PlayQueueAction::Pause) => {
-                        shared::PlayQueueEntry::Action(shared::PlayQueueAction::Pause)
-                    }
-                });
-                play_queue.current_playback = Some(track.clone());
-                handle_playback_request(
-                    play_queue,
-                    sink_client,
-                    mpris_client,
-                    tracker_client,
-                    shared::PlaybackRequest::LoadCurrentSong,
-                );
-            } else {
-                // Nothing else to run if not loading next
-            }
-        }
-        shared::PlaybackRequest::Next => {
-            if play_queue.play_queue.len() > 0 {
-                match play_queue.current_playback {
-                    Some(ref current_playback) => {
-                        play_queue.play_history.push(current_playback.clone())
-                    }
-                    None => (),
-                };
-
-                let track = play_queue.play_queue.remove(0);
-                //play_queue.current_playback = Some(state::CurrentPlayback::from_entry_zeroed(&track));
-                play_queue.current_second = 0;
-                play_queue.current_playback = Some(track.clone());
-                handle_playback_request(
-                    play_queue,
-                    sink_client,
-                    mpris_client,
-                    tracker_client,
-                    shared::PlaybackRequest::LoadCurrentSong,
-                );
-            } else {
-                match play_queue.current_playback {
-                    Some(ref current_playback) => {
-                        play_queue.play_history.push(current_playback.clone())
-                    }
-                    None => (),
-                };
-                play_queue.current_playback = None;
-                let _ = mpris_client.send(shared::MprisMessage::SetStopped);
-            }
-        }
-        shared::PlaybackRequest::Play => {
-            play_queue.playing = true;
-            let _ = sink_client.send(shared::SinkMessage::PlayButton);
-            let _ = mpris_client.send(shared::MprisMessage::SetPlaying);
-        }
-        shared::PlaybackRequest::Pause => {
-            play_queue.playing = false;
-            let _ = sink_client.send(shared::SinkMessage::PauseButton);
-            let _ = mpris_client.send(shared::MprisMessage::SetPaused);
-        }
-        shared::PlaybackRequest::InsertPause => {
-            let mut new_songs_to_queue = vec![shared::PlayQueueEntry::Action(
-                shared::PlayQueueAction::Pause,
-            )];
-            new_songs_to_queue.append(&mut play_queue.play_queue);
-            play_queue.play_queue = new_songs_to_queue;
-        }
-        shared::PlaybackRequest::SetVolume(new_volume) => {
-            let _ = sink_client.send(shared::SinkMessage::SetVolume(new_volume));
-        }
-        shared::PlaybackRequest::Close => {
-            let _ = sink_client.send(shared::SinkMessage::Close);
-            let _ = mpris_client.send(shared::MprisMessage::Close);
-        }
-    }
 }
