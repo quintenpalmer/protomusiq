a6ad989e26ae684920a3bc12a4426fe0ce4f5215
Quinten Palmer <quintenpalmer@protonmail.com>
2023-09-21 16:28:35 -0400

    Extract shared messages out to own module
    I also reordered the first two types, which will cause extra diff,
    whoops.

---
+++ src/app/src/shared/messages.rs
@@ -0,0 +1,73 @@
+use std::path;
+
+use crate::model;
+
+use super::state;
+
+#[derive(Debug, Clone)]
+pub enum GUIToBackendMessage {
+    BackendPlayback(PlaybackRequest),
+}
+
+#[derive(Debug, Clone)]
+pub enum PlaybackRequest {
+    Play,
+    Pause,
+    Prev,
+    Next,
+
+    InsertPause,
+
+    LoadCurrentSong,
+    PlaySongs(Vec<model::AugmentedTrack>),
+    InsertSongs(Vec<model::AugmentedTrack>, bool),
+    AppendSongs(Vec<model::AugmentedTrack>, bool),
+
+    SetVolume(f32),
+    Close,
+}
+
+#[derive(Debug, Clone)]
+pub enum BackendToGUIMessage {
+    PlayQueueState(state::PlayQueueInfo),
+}
+
+#[derive(Debug, Clone)]
+pub enum SinkMessage {
+    PlayButton,
+    PauseButton,
+    LoadSong(path::PathBuf, f32),
+    SetVolume(f32),
+    Close,
+}
+
+#[derive(Debug, Clone)]
+pub enum SinkCallbackMessage {
+    Playing,
+    Paused,
+    SecondElapsed,
+    SongEnded,
+}
+
+#[derive(Debug, Clone)]
+pub enum MprisMessage {
+    SetMetadata(String, String),
+    SetPlaying,
+    SetPaused,
+    SetStopped,
+    Close,
+}
+
+#[derive(Debug, Clone)]
+pub enum MprisCallbackMessage {
+    PlayPause,
+    Play,
+    Pause,
+    Prev,
+    Next,
+}
+
+#[derive(Debug, Clone)]
+pub enum TrackerMessage {
+    SongStarted(model::AugmentedTrack),
+}
--- src/app/src/shared/mod.rs
+++ src/app/src/shared/mod.rs
@@ -1,77 +1,7 @@
-use std::path;
-
-use crate::model;
-
 mod client;
+mod messages;
 mod state;
 
 pub use client::{Callback, Client};
+pub use messages::*;
 pub use state::{PlayQueueAction, PlayQueueEntry, PlayQueueInfo, PlayQueueTrack};
-
-#[derive(Debug, Clone)]
-pub enum PlaybackRequest {
-    Play,
-    Pause,
-    Prev,
-    Next,
-
-    InsertPause,
-
-    LoadCurrentSong,
-    PlaySongs(Vec<model::AugmentedTrack>),
-    InsertSongs(Vec<model::AugmentedTrack>, bool),
-    AppendSongs(Vec<model::AugmentedTrack>, bool),
-
-    SetVolume(f32),
-    Close,
-}
-
-#[derive(Debug, Clone)]
-pub enum GUIToBackendMessage {
-    BackendPlayback(PlaybackRequest),
-}
-
-#[derive(Debug, Clone)]
-pub enum BackendToGUIMessage {
-    PlayQueueState(PlayQueueInfo),
-}
-
-#[derive(Debug, Clone)]
-pub enum SinkMessage {
-    PlayButton,
-    PauseButton,
-    LoadSong(path::PathBuf, f32),
-    SetVolume(f32),
-    Close,
-}
-
-#[derive(Debug, Clone)]
-pub enum SinkCallbackMessage {
-    Playing,
-    Paused,
-    SecondElapsed,
-    SongEnded,
-}
-
-#[derive(Debug, Clone)]
-pub enum MprisMessage {
-    SetMetadata(String, String),
-    SetPlaying,
-    SetPaused,
-    SetStopped,
-    Close,
-}
-
-#[derive(Debug, Clone)]
-pub enum MprisCallbackMessage {
-    PlayPause,
-    Play,
-    Pause,
-    Prev,
-    Next,
-}
-
-#[derive(Debug, Clone)]
-pub enum TrackerMessage {
-    SongStarted(model::AugmentedTrack),
-}
