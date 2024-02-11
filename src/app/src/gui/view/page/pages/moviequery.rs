20ad11eab5431a95195b31cf19ae98bfe0046ab4
Quinten Palmer <quintenpalmer@protonmail.com>
2024-02-11 16:28:05 -0500

    Include matched movies in query results page

--- src/app/src/gui/view/page/pages/moviequery.rs
+++ src/app/src/gui/view/page/pages/moviequery.rs
@@ -1,17 +1,18 @@
-use iced::widget::{Column, Container};
+use iced::widget::{Column, Container, Row, Scrollable};
+use iced::Length;
 
 use crate::model;
 
 use crate::datastore::staticassets::embedded;
-use crate::gui::message::Message;
+use crate::gui::message::{user_nav_message, Message, NavMessage};
 use crate::state;
 
 use super::super::super::elements::*;
 
 pub fn movie_query<'a>(
-    _movie_library: &'a model::VideoLibraryState,
+    movie_library: &'a model::VideoLibraryState,
     state: &'a state::MovieQueryState,
-    _app_images: &embedded::AppImages,
+    app_images: &embedded::AppImages,
 ) -> (Vec<(String, Message)>, Container<'a, Message>) {
     let breadcrumbs = vec![];
 
@@ -26,11 +27,44 @@ pub fn movie_query<'a>(
         }
     };
 
+    let mut movie_list = Column::new().spacing(10);
+
+    for movie_key in state.matched_keys.iter() {
+        let movie = movie_library.get_movie(movie_key);
+
+        let movie_info = h1(movie.title.clone());
+
+        let movie_link = user_nav_message(NavMessage::MovieView(
+            movie.clone(),
+            model::MovieSize::SemiLarge,
+        ));
+
+        let movie_image_element = match movie_library.art.get_movie_cover(
+            model::MovieSize::Small,
+            model::MovieRelPath::from_metadata(&movie),
+        ) {
+            Some(movie_image_bytes) => movie_image(movie_image_bytes, model::MovieSize::Small),
+            None => movie_image(app_images.get_dvd_image().clone(), model::MovieSize::Small),
+        };
+
+        let movie_button = Row::new()
+            .push(dark_button(movie_image_element).on_press(movie_link.clone()))
+            .push(Container::new(dark_button(movie_info).on_press(movie_link)).width(Length::Fill));
+
+        movie_list = movie_list.push(movie_button);
+    }
+
+    let movies = Column::new()
+        .spacing(10)
+        .push(h1("Found Movies:"))
+        .push(Scrollable::new(movie_list));
+
     let body = Container::new(
         Column::new()
             .spacing(10)
             .push(h1("Movie Query"))
-            .push(input_query_element),
+            .push(input_query_element)
+            .push(movies),
     );
 
     (breadcrumbs, body)
