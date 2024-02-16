9f19ce86f694e22aaab2ead9c78a4e91d1b8d52e
Quinten Palmer <quintenpalmer@protonmail.com>
2024-02-15 19:42:40 -0500

    Introduce and leverage simplified `Mods` for keybindings
    This doesn't play nice over the abstraction over the `mod` key for macos
    users, but this app doesn't work for mac users at the moment anyways.

--- src/app/src/gui/subscription.rs
+++ src/app/src/gui/subscription.rs
@@ -26,53 +26,93 @@ pub fn keybinding_subscription_fn(
     key: keyboard::Key,
     modifiers: keyboard::Modifiers,
 ) -> Option<message::Message> {
-    match key.as_ref() {
-        keyboard::Key::Character("p") => Some(message::Message::Action(
+    let mod_enum = Mods::from_iced(modifiers);
+    match (key.as_ref(), mod_enum) {
+        (keyboard::Key::Character("p"), Mods::None) => Some(message::Message::Action(
             message::Action::TogglePlayQueueVisible,
         )),
-        keyboard::Key::Character("h") => {
-            if modifiers.shift() {
-                Some(message::Message::NavRelative(
-                    message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::First),
-                ))
-            } else {
-                Some(message::Message::NavRelative(
-                    message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Backwards),
-                ))
-            }
-        }
-        keyboard::Key::Character("l") => {
-            if modifiers.shift() {
-                Some(message::Message::NavRelative(
-                    message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Last),
-                ))
-            } else {
-                Some(message::Message::NavRelative(
-                    message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Forwards),
-                ))
-            }
-        }
-        keyboard::Key::Named(key::Named::Backspace) => Some(message::Message::HistoryNav),
-        keyboard::Key::Named(key::Named::BrowserBack) => Some(message::Message::HistoryNav),
-
-        keyboard::Key::Named(key::Named::ArrowUp) => Some(message::Message::NavRelative(
+        (keyboard::Key::Character("h"), Mods::Shift) => Some(message::Message::NavRelative(
             message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::First),
         )),
-        keyboard::Key::Named(key::Named::ArrowLeft) => {
-            if modifiers.alt() {
-                Some(message::Message::HistoryNav)
-            } else {
-                Some(message::Message::NavRelative(
-                    message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Backwards),
-                ))
-            }
-        }
-        keyboard::Key::Named(key::Named::ArrowRight) => Some(message::Message::NavRelative(
-            message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Forwards),
+        (keyboard::Key::Character("h"), Mods::None) => Some(message::Message::NavRelative(
+            message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Backwards),
         )),
-        keyboard::Key::Named(key::Named::ArrowDown) => Some(message::Message::NavRelative(
+        (keyboard::Key::Character("l"), Mods::Shift) => Some(message::Message::NavRelative(
             message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Last),
         )),
+        (keyboard::Key::Character("l"), Mods::None) => Some(message::Message::NavRelative(
+            message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Forwards),
+        )),
+        (keyboard::Key::Named(key::Named::Backspace), Mods::None) => {
+            Some(message::Message::HistoryNav)
+        }
+        (keyboard::Key::Named(key::Named::BrowserBack), Mods::None) => {
+            Some(message::Message::HistoryNav)
+        }
+        (keyboard::Key::Named(key::Named::ArrowLeft), Mods::Alt) => {
+            Some(message::Message::HistoryNav)
+        }
+
+        (keyboard::Key::Named(key::Named::ArrowUp), Mods::None) => {
+            Some(message::Message::NavRelative(
+                message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::First),
+            ))
+        }
+        (keyboard::Key::Named(key::Named::ArrowLeft), Mods::None) => {
+            Some(message::Message::HistoryNav)
+        }
+        (keyboard::Key::Named(key::Named::ArrowRight), Mods::None) => {
+            Some(message::Message::NavRelative(
+                message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Forwards),
+            ))
+        }
+        (keyboard::Key::Named(key::Named::ArrowDown), Mods::None) => {
+            Some(message::Message::NavRelative(
+                message::NavRelMsg::PagifiedMovement(message::PagifiedMovementMsg::Last),
+            ))
+        }
         _ => None,
+    }
+}
+
+enum Mods {
+    None,
+    Shift,
+    Control,
+    Alt,
+    Logo,
+    ShiftControl,
+    ShiftAlt,
+    ShiftLogo,
+    ControlAlt,
+    ControlLogo,
+    AltLogo,
+    ShiftControlAlt,
+    ShiftControlLogo,
+    ShiftAltLogo,
+    ControlAltLogo,
+    ShiftControlAltLogo,
+}
+
+impl Mods {
+    pub fn from_iced(mods: keyboard::Modifiers) -> Self {
+        match (mods.shift(), mods.control(), mods.alt(), mods.logo()) {
+            (false, false, false, false) => Mods::None,
+            (true, false, false, false) => Mods::Shift,
+            (false, true, false, false) => Mods::Control,
+            (false, false, true, false) => Mods::Alt,
+            (false, false, false, true) => Mods::Logo,
+            (true, true, false, false) => Mods::ShiftControl,
+            (true, false, true, false) => Mods::ShiftAlt,
+            (true, false, false, true) => Mods::ShiftLogo,
+            (false, true, true, false) => Mods::ControlAlt,
+            (false, true, false, true) => Mods::ControlLogo,
+            (false, false, true, true) => Mods::AltLogo,
+            (true, true, true, false) => Mods::ShiftControlAlt,
+            (true, true, false, true) => Mods::ShiftControlLogo,
+            (true, false, true, true) => Mods::ShiftAltLogo,
+            (false, true, true, true) => Mods::ControlAltLogo,
+            (true, true, true, true) => Mods::ShiftControlAltLogo,
+        }
     }
 }
