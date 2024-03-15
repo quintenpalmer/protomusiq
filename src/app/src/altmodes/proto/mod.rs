4777e9b460c2affa74c2c00fd6d1eb7daf609d6b
Quinten Palmer <quintenpalmer@protonmail.com>
2024-03-15 11:49:26 -0400

    Make `--prototype` a musicbrainz "main"

--- src/app/src/altmodes/proto/mod.rs
+++ src/app/src/altmodes/proto/mod.rs
@@ -15,6 +15,147 @@ use crate::datastore::sqlitebacked;
 pub enum Error {}
 
 pub fn entry_point() -> Result<(), Error> {
+    println!("prototype start");
+    let config_state = config::get_default_config();
+
+    let lib_path = config_state.library_path.clone();
+    println!("library path is: {:?}", lib_path);
+
+    println!("getting library");
+    let library = musiqlibrary::RawLibrary::new(lib_path.clone()).unwrap();
+    println!("got library");
+
+    let musicbrainz_artist_cache = cache::musicbrainz_artist_cache_dir();
+
+    println!("iterating over artists");
+    for artist in library.artists.values() {
+        grab_raw_musicbrainz_data(&musicbrainz_artist_cache, &artist.artist_info);
+
+        select_singular_artist_match(&musicbrainz_artist_cache, &artist.artist_info);
+    }
+
+    println!("going to try to reload all and compute lev distance");
+    for ml_artist in library.artists.values() {
+        match cache::read_musicbrainz_artist_match_file(
+            &musicbrainz_artist_cache,
+            ml_artist.artist_info.artist_name.clone(),
+        ) {
+            Some(artist) => {
+                let distance = model::functions::levenshtein(
+                    &artist.name.to_lowercase(),
+                    &ml_artist.artist_info.artist_name.to_lowercase(),
+                );
+                println!(
+                    "{}\t between \"{}\" and \"{}\"",
+                    distance, artist.name, ml_artist.artist_info.artist_name
+                );
+            }
+            None => {
+                println!(
+                    "1000\t between ??? and \"{}\"",
+                    ml_artist.artist_info.artist_name
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn grab_raw_musicbrainz_data(
+    musicbrainz_artist_cache: &path::PathBuf,
+    artist_info: &musiqlibrary::ArtistInfo,
+) {
+    println!(
+        "checking if cached musicbrainz exists for {}",
+        artist_info.artist_name,
+    );
+    if cache::raw_exists(&musicbrainz_artist_cache, artist_info.artist_name.clone()) {
+        println!(
+            "cached musicbrainz data does exist for {}",
+            artist_info.artist_name,
+        );
+    } else {
+        println!(
+            "querying musicbrainz for artist: {}",
+            artist_info.artist_name,
+        );
+        let json_str = musicbrainz::query_for_artist_raw(artist_info.artist_name.clone());
+
+        println!("writing json for: {}", artist_info.artist_name);
+        cache::write_musicbrainz_artist_cache_file(
+            &musicbrainz_artist_cache,
+            artist_info.artist_name.clone(),
+            json_str,
+        );
+
+        println!(
+            "done querying musicbrainz for artist: {}",
+            artist_info.artist_name
+        );
+
+        thread::sleep(time::Duration::new(2, 0))
+    }
+}
+
+pub fn select_singular_artist_match(
+    musicbrainz_artist_cache: &path::PathBuf,
+    artist_info: &musiqlibrary::ArtistInfo,
+) {
+    println!(
+        "trying to find singular artist match: {}",
+        artist_info.artist_name,
+    );
+    if cache::match_exists(&musicbrainz_artist_cache, artist_info.artist_name.clone()) {
+        println!(
+            "matched single musicbrainz entry does exist for artist: {}",
+            artist_info.artist_name,
+        );
+    } else {
+        println!(
+            "finding single musicbrainz match for artist: {}",
+            artist_info.artist_name,
+        );
+        let json_str = cache::read_musicbrainz_artist_cache_file(
+            &musicbrainz_artist_cache,
+            artist_info.artist_name.clone(),
+        );
+
+        let artist_results = musicbrainz::ArtistListResult::from_json(json_str);
+
+        let mut found_100_matches = Vec::new();
+
+        for artist in artist_results.artists.into_iter() {
+            if artist.score == 100 {
+                found_100_matches.push(artist);
+            }
+        }
+
+        match found_100_matches.as_slice() {
+            &[ref single_match] => cache::write_musicbrainz_artist_match_file(
+                &musicbrainz_artist_cache,
+                artist_info.artist_name.clone(),
+                single_match,
+            ),
+            &[] => println!(
+                "found no 100 matches for this artist: {}",
+                artist_info.artist_name
+            ),
+            _ => println!(
+                "found multiple 100 matches for this artist: {}",
+                artist_info.artist_name
+            ),
+        };
+
+        println!(
+            "done finding single artist match for: {}",
+            artist_info.artist_name
+        );
+    }
+}
+
+#[allow(unused)]
+pub fn sql_mode() -> Result<(), Error> {
     let config_state = config::get_default_config();
 
     let mut keep_going = true;
