18d2bfd642012a258d6bbea7f4a2d58f79c95c3b
Quinten Palmer <quintenpalmer@protonmail.com>
2024-04-29 21:40:27 -0400

    Store and expose prefix dir for GBA game library

--- src/app/src/model/gamelibrary/mod.rs
+++ src/app/src/model/gamelibrary/mod.rs
@@ -62,17 +62,20 @@ fn clean_filename_to_game_name(path: &path::PathBuf) -> String {
 }
 
 pub struct GameLibrary {
+    pub gba_prefix_dir: Option<path::PathBuf>,
     pub gba_rom_paths: Vec<GBAGame>,
 }
 
 impl GameLibrary {
-    pub fn new<P: AsRef<path::Path>>(gba_path: &Option<P>) -> Self {
+    pub fn new<P: AsRef<path::Path> + Clone>(gba_path: &Option<P>) -> Self {
         let gba_rom_paths = match gba_path {
             Some(actual_gba_path) => games::gba::scan_for_gba_rom_files(&actual_gba_path).unwrap(),
             None => Vec::new(),
         };
 
         GameLibrary {
+            gba_prefix_dir: <std::option::Option<P> as Clone>::clone(gba_path)
+                .map(|x| x.as_ref().to_path_buf()),
             gba_rom_paths: gba_rom_paths.into_iter().map(|x| GBAGame::new(x)).collect(),
         }
     }
@@ -87,6 +90,10 @@ impl GameLibraryState {
         GameLibraryState {
             games: game_library,
         }
+    }
+
+    pub fn get_gba_prefix_path(&self) -> &Option<path::PathBuf> {
+        &self.games.gba_prefix_dir
     }
 
     pub fn get_gba_rom_paths(&self) -> &Vec<GBAGame> {
