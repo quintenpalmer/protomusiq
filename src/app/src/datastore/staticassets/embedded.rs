c038969a20b962d755113e1020cb5644b34e5e1e
Quinten Palmer <quintenpalmer@protonmail.com>
2024-05-01 17:50:11 -0400

    Introduce and expose tag png image

--- src/app/src/datastore/staticassets/embedded.rs
+++ src/app/src/datastore/staticassets/embedded.rs
@@ -5,6 +5,7 @@ pub struct AppImages {
     artists_image: Vec<u8>,
     albums_image: Vec<u8>,
     tracks_image: Vec<u8>,
+    tag_image: Vec<u8>,
     playlists_image: Vec<u8>,
     search_image: Vec<u8>,
     settings_image: Vec<u8>,
@@ -20,6 +21,7 @@ impl AppImages {
             include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/embedded/albums.png"));
         let tracks_image =
             include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/embedded/tracks.png"));
+        let tag_image = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/embedded/tag.png"));
         let playlists_image = include_bytes!(concat!(
             env!("CARGO_MANIFEST_DIR"),
             "/embedded/playlists.png"
@@ -44,6 +46,7 @@ impl AppImages {
             artists_image: artists_image.to_vec(),
             albums_image: albums_image.to_vec(),
             tracks_image: tracks_image.to_vec(),
+            tag_image: tag_image.to_vec(),
             playlists_image: playlists_image.to_vec(),
             search_image: search_image.to_vec(),
             settings_image: settings_image.to_vec(),
@@ -62,6 +65,10 @@ impl AppImages {
 
     pub fn get_tracks_image(&self) -> &Vec<u8> {
         &self.tracks_image
+    }
+
+    pub fn get_tag_image(&self) -> &Vec<u8> {
+        &self.tag_image
     }
 
     pub fn get_playlists_image(&self) -> &Vec<u8> {
