f2b81c9972cc24dae2421e1f367818613d412b63
Quinten Palmer <quintenpalmer@protonmail.com>
2024-06-08 13:12:35 -0400

    Introduce function to get full console name
    This will be used at the very least to match our image filestructure.

--- src/app/src/model/gamelibrary/consoles.rs
+++ src/app/src/model/gamelibrary/consoles.rs
@@ -7,3 +7,17 @@ pub enum GameConsole {
     GameCube,
     Wii,
 }
+
+impl GameConsole {
+    pub fn full_name(&self) -> String {
+        match self {
+            GameConsole::GameBoyAdvance => "Game Boy Advance",
+            GameConsole::NintendoDS => "Nintendo DS",
+            GameConsole::SNES => "Super Nintendo Entertainment System",
+            GameConsole::Nintendo64 => "Nintendo 64",
+            GameConsole::GameCube => "GameCube",
+            GameConsole::Wii => "Wii",
+        }
+        .to_string()
+    }
+}
