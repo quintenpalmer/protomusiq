38be9938aa953f5cf4d826a4adda397ee43b05d1
Quinten Palmer <quintenpalmer@protonmail.com>
2024-07-13 13:21:21 -0400

    `musiqassistant` generates script to cp GBA images to canonical image dir

--- src/assistant/src/main.rs
+++ src/assistant/src/main.rs
@@ -1,3 +1,48 @@
+use std::env;
+use std::path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("must provide the source image path");
+        return;
+    }
+
+    let source_image_path = Some(path::PathBuf::from(args[1].clone()));
+
+    let config_state = musiqcore::model::app::AppConfigState::get_default();
+
+    let game_library =
+        musiqcore::model::gl::GameLibrary::new(&source_image_path, &config_state.games);
+
+    let game_library_state = musiqcore::model::gl::GameLibraryState::new(game_library);
+
+    match (
+        game_library_state.get_gba_prefix_path(),
+        game_library_state.get_gba_rom_paths(),
+    ) {
+        (Some(rom_prefix), Some(rom_paths)) => {
+            let console = musiqcore::model::gl::consoles::GameConsole::GameBoyAdvance;
+
+            for game in rom_paths.iter() {
+                let source_image_path = game.image_path.clone();
+
+                let dest_prefix = config_state.games.clone().unwrap().image_path;
+
+                let dest_console_piece = path::PathBuf::from(console.full_name());
+
+                let dest_base_name = game
+                    .path
+                    .strip_prefix(rom_prefix)
+                    .unwrap()
+                    .with_extension("png");
+
+                let dest_image_path = dest_prefix.join(dest_console_piece).join(dest_base_name);
+
+                println!("cp {:?} {:?}", source_image_path, dest_image_path);
+            }
+        }
+        _ => println!("skipping gbas"),
+    }
 }
