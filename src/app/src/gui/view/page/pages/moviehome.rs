ab40cb70db275607efd1bb5f736e5a5a7878e75e
Quinten Palmer <quintenpalmer@protonmail.com>
2024-08-13 13:22:41 -0400

    Wrap movie home content in column
    We are about to add a second column, and as the variable name suggests,
    we intended for this to be a column the whole time. It was also
    expected to be a column from the scrollable wrapping it lower in the
    file as well.

--- src/app/src/gui/view/page/pages/moviehome.rs
+++ src/app/src/gui/view/page/pages/moviehome.rs
@@ -11,48 +11,50 @@ use super::super::super::consts;
 use super::super::super::elements::*;
 
 pub fn movie_home<'a>(app_images: &embedded::AppImages) -> Container<'a, Message> {
-    let body_column = Row::new()
-        .push(
-            dark_button(Container::new(bottom_label(
-                album_image(app_images.get_dvd_image().clone(), model::AlbumSize::Small).into(),
-                bright_paragraph(common::abr_str(
-                    "Movie List".to_string(),
-                    consts::ICON_STR_LENGTH,
-                )),
-            )))
-            .on_press(
-                message::MovieNavMessage::MovieList(
-                    0,
-                    model::MovieSortKey::preferred_home(),
-                    model::MovieSortKey::preferred_home().default_order(),
-                )
-                .into_message(),
+    let body_column = Column::new().push(
+        Row::new()
+            .push(
+                dark_button(Container::new(bottom_label(
+                    album_image(app_images.get_dvd_image().clone(), model::AlbumSize::Small).into(),
+                    bright_paragraph(common::abr_str(
+                        "Movie List".to_string(),
+                        consts::ICON_STR_LENGTH,
+                    )),
+                )))
+                .on_press(
+                    message::MovieNavMessage::MovieList(
+                        0,
+                        model::MovieSortKey::preferred_home(),
+                        model::MovieSortKey::preferred_home().default_order(),
+                    )
+                    .into_message(),
+                ),
+            )
+            .push(
+                dark_button(Container::new(bottom_label(
+                    album_image(
+                        app_images.get_search_image().clone(),
+                        model::AlbumSize::Small,
+                    )
+                    .into(),
+                    bright_paragraph(common::abr_str(
+                        "Movie Query".to_string(),
+                        consts::ICON_STR_LENGTH,
+                    )),
+                )))
+                .on_press(message::MovieNavMessage::MovieQuery(None).into_message()),
+            )
+            .push(
+                dark_button(Container::new(bottom_label(
+                    album_image(app_images.get_tag_image().clone(), model::AlbumSize::Small).into(),
+                    bright_paragraph(common::abr_str(
+                        "Movie Attrs".to_string(),
+                        consts::ICON_STR_LENGTH,
+                    )),
+                )))
+                .on_press(message::MovieNavMessage::MovieAttributes(None).into_message()),
             ),
-        )
-        .push(
-            dark_button(Container::new(bottom_label(
-                album_image(
-                    app_images.get_search_image().clone(),
-                    model::AlbumSize::Small,
-                )
-                .into(),
-                bright_paragraph(common::abr_str(
-                    "Movie Query".to_string(),
-                    consts::ICON_STR_LENGTH,
-                )),
-            )))
-            .on_press(message::MovieNavMessage::MovieQuery(None).into_message()),
-        )
-        .push(
-            dark_button(Container::new(bottom_label(
-                album_image(app_images.get_tag_image().clone(), model::AlbumSize::Small).into(),
-                bright_paragraph(common::abr_str(
-                    "Movie Attrs".to_string(),
-                    consts::ICON_STR_LENGTH,
-                )),
-            )))
-            .on_press(message::MovieNavMessage::MovieAttributes(None).into_message()),
-        );
+    );
 
     let body = Container::new(
         Column::new()
