336c8cd92a1648c4c0db7557b6c2ef18869e1736
Quinten Palmer <quintenpalmer@protonmail.com>
2024-09-05 15:09:59 -0400

    Tracker information is read with a `threadpool`

--- src/app/src/datastore/jsonbacked/tracker/readwrite.rs
+++ src/app/src/datastore/jsonbacked/tracker/readwrite.rs
@@ -2,6 +2,7 @@ use std::collections::BTreeMap;
 use std::fs;
 use std::io;
 use std::path::PathBuf;
+use std::sync::mpsc;
 
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
@@ -114,30 +115,76 @@ pub fn list_all_tracker_records(
 
     let tracker_files_dir = localfs::build_tree_for_dirs(app_data_path, vec!["data", "tracker"]);
 
+    let child_paths = fs::read_dir(tracker_files_dir.clone())
+        .unwrap()
+        .into_iter()
+        .collect::<Vec<_>>();
+    println!("with these many tracker files: {}", child_paths.len());
+    for tracker_file in child_paths {
+        println!("this tracker file: {:?}", tracker_file);
+    }
+
+    let num_threads = std::thread::available_parallelism()
+        .map(|x| x.into())
+        .unwrap_or(1);
+
+    println!("creating thread pool with {} threads", num_threads);
+
+    let tpool = threadpool::ThreadPool::new(num_threads);
+    let (tx, rx) = mpsc::channel();
+
+    let mut total_to_read = 0;
+
     for tracker_file in fs::read_dir(tracker_files_dir).unwrap() {
+        let tx = tx.clone();
+
         let tracker_file = tracker_file.unwrap().path();
-        println!("tracker_file: {:?}", tracker_file);
-        if allowed_files.is_allowed(&tracker_file) {
-            match tracker_file.extension().map(|x| x.to_str()) {
-                Some(Some("json")) => {
-                    let current_tracks: RawTrackedPayload =
-                        common::maybe_get_existing_raw_data(&tracker_file).unwrap();
-                    for (current_track, mut current_track_count) in
-                        current_tracks.tracks.into_iter()
-                    {
-                        all_tracks
-                            .entry(current_track)
-                            .or_insert(Vec::new())
-                            .append(&mut current_track_count);
-                    }
+        let should_read = allowed_files.is_allowed(&tracker_file)
+            && match tracker_file.extension().map(|x| x.to_str()) {
+                Some(Some("json")) => true,
+                Some(Some(ext)) => {
+                    println!("skipping non json file ({}) in data/tracker/", ext);
+                    false
                 }
-                Some(Some(ext)) => println!("skipping non json file ({}) in data/tracker/", ext),
-                Some(None) => println!("skipping non json file in data/tracker/"),
-                None => println!("skipping non json file in data/tracker/"),
-            }
-        } else {
-            println!("skipping file which was not allowed from config");
+                Some(None) => {
+                    println!("skipping non json file in data/tracker/");
+                    false
+                }
+                None => {
+                    println!("skipping non json file in data/tracker/");
+                    false
+                }
+            };
+
+        if should_read {
+            total_to_read += 1;
         }
+
+        tpool.execute(move || {
+            println!("tracker_file: {:?}", tracker_file);
+            if should_read {
+                println!("reading json file ({:?}) in data/tracker/", tracker_file);
+                let current_tracks: RawTrackedPayload =
+                    common::maybe_get_existing_raw_data(&tracker_file).unwrap();
+                for (current_track, current_track_count) in current_tracks.tracks.into_iter() {
+                    tx.send((current_track, current_track_count))
+                        .expect("please let the recv be listening");
+                }
+            } else {
+                println!("skipping file which was not allowed from config");
+            }
+        });
+    }
+
+    drop(tx);
+
+    println!("total to read will be: {}", total_to_read);
+
+    for (current_track, mut current_track_count) in rx {
+        all_tracks
+            .entry(current_track)
+            .or_insert(Vec::new())
+            .append(&mut current_track_count);
     }
     all_tracks
 }
