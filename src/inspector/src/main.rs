02ee0c89bfd7fd8dcafcbdbc4d2475910ffd3192
Quinten Palmer <quintenpalmer@protonmail.com>
2024-09-09 13:47:35 -0400

    Use proper structure for command handling
    This allows sub-commands easily too. Note that the help text has
    deteriorated though; we will want to improve that back up to at least
    where it was.

--- src/inspector/src/main.rs
+++ src/inspector/src/main.rs
@@ -19,8 +19,60 @@ fn help_text(cmds: &Vec<String>, extra: String) {
     panic!("exiting with non-zero status");
 }
 
+pub struct ParentCommand {
+    pub sub_commands: BTreeMap<&'static str, Command>,
+}
+
+pub enum Command {
+    Parent(ParentCommand),
+    Specific(Box<dyn AppCmd>),
+}
+
+impl Command {
+    pub fn new_parent(btree_map: BTreeMap<&'static str, Command>) -> Self {
+        Command::Parent(ParentCommand {
+            sub_commands: btree_map,
+        })
+    }
+
+    pub fn operate_on_args(&self, mut args: Vec<String>) {
+        println!("operating on: {:?}", args);
+        if args.len() < 1 {
+            panic!("no arguments provided, and I don't actually know how to provide a useful help text");
+        }
+        match self {
+            Command::Specific(inner) => {
+                if args.len() < 1 {
+                    panic!("no arguments provided, this command currently just needs a path as an argument");
+                }
+                let lib_path = args[0].clone();
+                inner.operate(Path::new(lib_path.as_str()).to_path_buf())
+            }
+            Command::Parent(parent) => {
+                let printable_cmds = parent.sub_commands.keys().map(|x| x.to_string()).collect();
+
+                if args.len() < 1 {
+                    help_text(&printable_cmds, "did not provide any command".to_string())
+                }
+
+                let parsed_cmd = args.remove(0);
+
+                println!("parsed cmd: {}", parsed_cmd);
+
+                match parent.sub_commands.get(parsed_cmd.to_string().as_str()) {
+                    Some(f) => f.operate_on_args(args),
+                    None => help_text(
+                        &printable_cmds,
+                        format!("unknown command: {}", parsed_cmd.clone()),
+                    ),
+                }
+            }
+        }
+    }
+}
+
 fn main() {
-    let available_commands: BTreeMap<&'static str, Box<dyn AppCmd>> = {
+    let available_commands: BTreeMap<&'static str, Command> = {
         let inner: Vec<(&'static str, Box<dyn AppCmd>)> = vec![
             ("conflicts", Box::new(ConflictLister {})),
             ("tracks", Box::new(TrackLister {})),
@@ -39,28 +91,17 @@ fn main() {
         inner
     }
     .into_iter()
+    .map(|(x, y)| (x, Command::Specific(y)))
     .collect();
 
-    let printable_cmds = available_commands.keys().map(|x| x.to_string()).collect();
+    let command = Command::new_parent(available_commands);
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        help_text(&printable_cmds, "".to_string())
-    }
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
 
-    let parsed_cmd = args[1].clone();
-    let lib_path = args[2].clone();
+    println!("args: {:?}", args);
 
-    println!("parsed cmd: {}", parsed_cmd);
-    println!("lib path: {}", lib_path);
-
-    match available_commands.get(parsed_cmd.to_string().as_str()) {
-        Some(f) => f.operate(Path::new(lib_path.as_str()).to_path_buf()),
-        _ => help_text(
-            &printable_cmds,
-            format!("unknown command: {}", parsed_cmd.clone()),
-        ),
-    }
+    command.operate_on_args(args)
 }
 
 pub trait AppCmd {
