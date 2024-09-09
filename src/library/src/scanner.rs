07e8e8a54af7d233f9b4004aab7b69bb6140df5b
Quinten Palmer <quintenpalmer@protonmail.com>
2024-09-09 01:00:13 -0400

    Convert `find_files` to use `threadpool`

--- src/library/src/scanner.rs
+++ src/library/src/scanner.rs
@@ -1,6 +1,7 @@
 use std::fs::{self};
 use std::io;
 use std::path::Path;
+use std::sync::mpsc;
 
 use crate::model::{FullTrackMetadata, TrackPathInfo};
 
@@ -94,64 +95,88 @@ pub fn find_files<O: AsRef<Path>>(orig_prefix: &O) -> io::Result<Vec<FullTrackMe
 
     let mut metadata_map = Vec::new();
 
+    let num_threads = std::thread::available_parallelism()
+        .map(|x| x.into())
+        .unwrap_or(1);
+
+    println!("creating thread pool with {} threads", num_threads);
+
+    let tpool = threadpool::ThreadPool::new(num_threads);
+    let (tx, rx) = mpsc::channel();
+
     for path in files.into_iter() {
-        let maybe_parser: Option<Box<dyn parser::MetadataParser>> = match path
-            .path
-            .extension()
-            .map(|a| a.to_str().map(|x| x.to_lowercase()))
-        {
-            Some(inner_opt) => {
-                match inner_opt {
-                    Some(ext) => match ext.as_str() {
-                        "flac" => Some(Box::new(
-                            parser::FlacMetadataParser::new(path.path).unwrap(),
-                        )),
-                        "mp3" => Some(Box::new(parser::ID3MetadataParser::new(path.path).unwrap())),
-                        "m4a" => Some(Box::new(
-                            parser::MP4AMetadataParser::new(path.path).unwrap(),
-                        )),
-                        // these files are common to see, so we don't log if we see them
-                        // consider extracting this out to an 'silent-ignore-suffix' list
-                        "png" => None,
-                        "txt" => None,
-                        "rtf" => None,
-                        "jpg" => None,
-                        "gif" => None,
-                        "pdf" => None,
-                        "webp" => None,
-                        unexpected_ext => {
+        let tx = tx.clone();
+
+        let orig_prefix = orig_prefix.as_ref().to_path_buf().clone();
+
+        tpool.execute(move || {
+            let maybe_parser: Option<Box<dyn parser::MetadataParser>> = match path
+                .path
+                .extension()
+                .map(|a| a.to_str().map(|x| x.to_lowercase()))
+            {
+                Some(inner_opt) => {
+                    match inner_opt {
+                        Some(ext) => match ext.as_str() {
+                            "flac" => Some(Box::new(
+                                parser::FlacMetadataParser::new(path.path).unwrap(),
+                            )),
+                            "mp3" => {
+                                Some(Box::new(parser::ID3MetadataParser::new(path.path).unwrap()))
+                            }
+                            "m4a" => Some(Box::new(
+                                parser::MP4AMetadataParser::new(path.path).unwrap(),
+                            )),
+                            // these files are common to see, so we don't log if we see them
+                            // consider extracting this out to an 'silent-ignore-suffix' list
+                            "png" => None,
+                            "txt" => None,
+                            "rtf" => None,
+                            "jpg" => None,
+                            "gif" => None,
+                            "pdf" => None,
+                            "webp" => None,
+                            unexpected_ext => {
+                                eprintln!(
+                                    "no music metadata parsed for extension {}\t(path: {})",
+                                    unexpected_ext,
+                                    path.path.display()
+                                );
+                                None
+                            }
+                        },
+                        None => {
                             eprintln!(
-                                "no music metadata parsed for extension {}\t(path: {})",
-                                unexpected_ext,
-                                path.path.display()
-                            );
-                            None
-                        }
-                    },
-                    None => {
-                        eprintln!(
                             "could not resolve file extension, let me know if you ever see this"
                         );
-                        None
+                            None
+                        }
                     }
                 }
-            }
-            None => {
-                eprintln!(
-                    "no music metadata parsed file with no extension\t(path: {})",
-                    path.path.display()
-                );
-                None
-            }
-        };
+                None => {
+                    eprintln!(
+                        "no music metadata parsed file with no extension\t(path: {})",
+                        path.path.display()
+                    );
+                    None
+                }
+            };
 
-        match maybe_parser {
-            Some(parser) => {
-                let track_info = parser::resolve_metadata_from_parser(orig_prefix, parser);
-                metadata_map.push(track_info);
-            }
-            None => (),
-        };
+            match maybe_parser {
+                Some(parser) => {
+                    let track_info = parser::resolve_metadata_from_parser(&orig_prefix, parser);
+                    tx.send(track_info)
+                        .expect("I hope the music metadata scanner rx is receiving");
+                }
+                None => (),
+            };
+        });
+    }
+
+    drop(tx);
+
+    for music_metadata in rx {
+        metadata_map.push(music_metadata);
     }
 
     Ok(metadata_map)
