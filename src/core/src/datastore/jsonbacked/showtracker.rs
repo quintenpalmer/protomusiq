f6449240e06258528154db513e56315cd867c3a6
Quinten Palmer <quintenpalmer@protonmail.com>
2025-01-16 14:22:51 -0500

    Introduce unused (yet) cached most recently viewed episodes

--- src/core/src/datastore/jsonbacked/showtracker.rs
+++ src/core/src/datastore/jsonbacked/showtracker.rs
@@ -1,5 +1,5 @@
 use std::collections::BTreeMap;
-use std::{fs, io, path};
+use std::{cmp, fs, io, path};
 
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
@@ -9,21 +9,42 @@ use crate::datastore::jsonbacked::common;
 pub struct ShowTracker {
     show_tracker_json_file_path: path::PathBuf,
     tracked_show_views: BTreeMap<musiqlibrary::shows::ShowEpisodeKey, Vec<DateTime<Local>>>,
+    cached_most_recently_viewed_shows: BTreeMap<String, musiqlibrary::shows::ShowEpisodeKey>,
 }
 
 impl ShowTracker {
     pub fn new<P: AsRef<path::Path>>(app_data_path: P) -> Self {
         let app_data_path = app_data_path.as_ref().to_path_buf();
-        let (tracked_show_views, show_tracker_json_file_path): (RawShowTrackedPayload, _) =
+        let (tracked_show_views_raw, show_tracker_json_file_path): (RawShowTrackedPayload, _) =
             common::bootstrap_raw_data(
                 &app_data_path,
                 vec!["data", "showtracker", "timestamps.json"],
             );
 
+        let mut cached_most_recently_viewed_shows = BTreeMap::new();
+
+        for (episode_key, _timestamp) in tracked_show_views_raw.views.iter() {
+            let found_episode = cached_most_recently_viewed_shows
+                .entry(episode_key.show.clone())
+                .or_insert(episode_key.clone());
+
+            *found_episode = cmp::max(found_episode.clone(), episode_key.clone());
+        }
+
+        let tracked_show_views = tracked_show_views_raw.to_btree_map();
+
         ShowTracker {
             show_tracker_json_file_path,
-            tracked_show_views: tracked_show_views.to_btree_map(),
+            tracked_show_views,
+            cached_most_recently_viewed_shows,
         }
+    }
+
+    pub fn get_show_most_recently_viewed(
+        &self,
+        show_name: &String,
+    ) -> Option<&musiqlibrary::shows::ShowEpisodeKey> {
+        self.cached_most_recently_viewed_shows.get(show_name)
     }
 
     pub fn get_view_count_for_episode(&self, key: musiqlibrary::shows::ShowEpisodeKey) -> usize {
